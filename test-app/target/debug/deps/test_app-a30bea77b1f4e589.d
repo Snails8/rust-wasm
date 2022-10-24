103c97741cb1af5818671765d5222c12e9446f83
Kaito Suzuki <snails8d@gmail.com>
2022-10-24 21:21:48 +0900

    init: test prj の作成

---
+++ test-app/target/debug/deps/test_app-a30bea77b1f4e589.d
@@ -0,0 +1,5 @@
+/app/test-app/target/debug/deps/test_app-a30bea77b1f4e589: src/main.rs
+
+/app/test-app/target/debug/deps/test_app-a30bea77b1f4e589.d: src/main.rs
+
+src/main.rs:
---
+++ test-app/target/debug/test-app.d
@@ -0,0 +1,1 @@
+/app/test-app/target/debug/test-app: /app/test-app/src/main.rs
